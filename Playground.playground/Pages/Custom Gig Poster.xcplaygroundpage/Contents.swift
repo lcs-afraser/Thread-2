beb584dcaa9d6bb6ff7318b06a8cc5abb194049c
Alistair <afraser@lcs.on.ca>
2022-02-03 08:58:07 -0500

    Done my poster

--- Playground.playground/Pages/Custom Gig Poster.xcplaygroundpage/Contents.swift
+++ Playground.playground/Pages/Custom Gig Poster.xcplaygroundpage/Contents.swift
@@ -63,20 +63,23 @@ for width in stride(from: -100, through: 800, by: 16) {
     //lines
     canvas.defaultLineWidth = 1
     canvas.lineColor = .white
-    canvas.drawLine(from: Point (x: width - 100, y: 150), to: Point (x: 200, y: 1000))
+    canvas.drawLine(from: Point (x: width - 200, y: 0), to: Point (x: 200, y: 1500))
 }
 
-//BIg Cross
+//Big Cross
 canvas.fillColor = .black
 canvas.drawShapesWithFill = true
+canvas.drawShapesWithBorders = true
+canvas.defaultBorderWidth = 10
+canvas.borderColor = .red
 var bigCross: [Point] = []
 bigCross.append(Point (x: 150, y: 150))
 bigCross.append(Point (x: 150, y:  400))
 bigCross.append(Point (x: 50, y: 400))
 bigCross.append(Point (x: 50, y: 500))
 bigCross.append(Point (x: 150, y: 500))
-bigCross.append(Point (x: 150, y: 600))
-bigCross.append(Point (x: 250, y: 600))
+bigCross.append(Point (x: 150, y: 595))
+bigCross.append(Point (x: 250, y: 595))
 bigCross.append(Point (x: 250, y: 500))
 bigCross.append(Point (x: 350, y: 500))
 bigCross.append(Point (x: 350, y: 400))
@@ -85,29 +88,64 @@ bigCross.append(Point (x: 250, y:  150))
 canvas.drawCustomShape(with: bigCross)
 
 //Stride
-for height in stride(from: 150, through: 600, by: 50) {
-    for width in stride(from: 150, through: 250, by: 50) {
-//Triangles
-     //   canvas.fillColor = .white
-       // var bigCross: [Point] = []
-        //bigCross.append(Point (x: width - 0, y: height - 15))
-        //bigCross.append(Point (x: width - 0, y:  height - 0))
-        //bigCross.append(Point (x: width - 0, y: height - 0))
-        //bigCross.append(Point (x: width - 10, y: height - 15))
-       // bigCross.append(Point (x: width - 50, y: height - 25))
-       // bigCross.append(Point (x: width - 50, y: height - 35))
-       // bigCross.append(Point (x: width - 30, y: height - 35))
-       // bigCross.append(Point (x: width - 30, y: height - 50))
-        //bigCross.append(Point (x: width - 30, y: height - 25))
-      //  bigCross.append(Point (x: width - 0, y: height - 25))
-       // bigCross.append(Point (x: width - 0, y: height - 25))
-        //bigCross.append(Point (x: width, y:  height))
-      //  canvas.drawCustomShape(with: bigCross)
+for height in stride(from: 160, through: 590, by: 20) {
+    for width in stride(from: 160, through: 250, by: 20) {
+        // Bottom Pattern
+        if height - width <= 160 {
+            canvas.lineColor = .red
+        }
+        else {
+            canvas.lineColor = .white
+        }
+        canvas.defaultLineWidth = 10
+        canvas.drawLine(from: Point (x: width, y: height), to: Point (x: width, y: height))
     }
 }
-        
-// Manditory
-canvas.drawAxes(withScale: true, by: 50, color: .green)
+//Stride
+for height in stride(from: 410, through: 490, by: 20) {
+    for width in stride(from: 60, through: 140, by: 20) {
+        // Bottom Pattern
+        if height - width <= 160 {
+            canvas.lineColor = .red
+        }
+        else {
+            canvas.lineColor = .white
+        }
+        canvas.defaultLineWidth = 10
+        canvas.drawLine(from: Point (x: width, y: height), to: Point (x: width, y: height))
+    }
+}
+//Stride
+for height in stride(from: 410, through: 490, by: 20) {
+    for width in stride(from: 260, through: 340, by: 20) {
+        // Bottom Pattern
+        if height - width <= 160 {
+            canvas.lineColor = .red
+        }
+        else {
+            canvas.lineColor = .white
+        }
+        canvas.defaultLineWidth = 10
+        canvas.drawLine(from: Point (x: width, y: height), to: Point (x: width, y: height))
+    }
+}
+//Text
+canvas.drawShapesWithBorders = false
+canvas.drawShapesWithFill = true
+canvas.fillColor = .black
+canvas.drawRectangle(at: Point (x: 86, y: 15), width: 220, height: 120)
+canvas.textColor = .red
+canvas.drawText(message: "YEEZUS", at: Point (x: 100, y: 80), size: 50, kerning: 0)
+canvas.textColor = .white
+canvas.drawText(message: "KANYE WEST", at: Point (x: 100, y: 50), size: 30, kerning: 0)
+canvas.textColor = .red
+canvas.drawText(message: "Centre Bell, Montreal, Quebec, Canada", at: Point (x: 87, y: 35), size: 12, kerning: 0)
+canvas.textColor = .white
+canvas.drawText(message: "Feb 17th, 2014", at: Point (x: 160, y: 18), size: 12, kerning: 0)
+
+
+//Manditory
+//canvas.drawAxes(withScale: true, by: 50, color: .white)
 
 canvas.highPerformance = false
 /*:
